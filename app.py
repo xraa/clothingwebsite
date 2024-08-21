f5e383a5ded2f55da1a93aeea1661f884ff52b12
xraa <166733917+xraa@users.noreply.github.com>
2024-08-21 17:58:22 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
+
+# - - - - - HEADER SECTION - - - - 
+st.subheader("Hi, I am Leandre :wave:")
+st.title("A cool dude who likes coding")
+st.write("I enjoy basketball and other fun sports.")
+st.write("I will be updating this page, and uplaoding my projects here. My discord is ____. Feel free to contact me whenever")
